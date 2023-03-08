87b764491b15aa957337c997a4adb70ea1b4e145
LemonSauc3 <riley@boyder.ca>
2023-03-08 01:03:18 -0500

    mobile and desktop changes for navbar

--- components/navbar.js
+++ components/navbar.js
@@ -6,11 +6,17 @@ const Navbar = () => {
   const [title, setTitle] = useState("Home");
 
   return (
-    <>
+    <div className="bg-gray-600 border-2 m-1 rounded-md sm:h-12">
       {
         // Mobile menu
       }
-      <div className="flex flex-row md:hidden">
+      <div className="flex flex-row justify-between md:hidden p-2">
+        <Link className="text-3xl text-white" href="/">
+          Tri-City
+        </Link>
+        <h1 className="text-3xl text-white text-center outline-none">
+          {title}
+        </h1>
         <button
           onClick={(e) => {
             e.preventDefault();
@@ -20,7 +26,7 @@ const Navbar = () => {
           }}
         >
           <svg
-            className="w-10 h-10 text-gray-500"
+            className="w-10 h-10 text-white"
             x-show="!showMenu"
             fill="none"
             stroke-linecap="round"
@@ -32,46 +38,48 @@ const Navbar = () => {
             <path d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </button>
-        <div className="hidden mobile-menu">
-          <ul className="flex flex-col space-y-2">
-            {routes.map((route) => (
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTitle(route.name);
-                  setNavOpen(!navOpen);
-                  const menu = document.querySelector(".mobile-menu");
-                  menu.classList.toggle("hidden");
-                }}
+      </div>
+      <div className="hidden mobile-menu">
+        <ul className="flex flex-col space-y-2">
+          {routes.map((route) => (
+            <button
+              className="text-lg text-white outline-none"
+              onClick={(e) => {
+                e.preventDefault();
+                setTitle(route.name);
+                setNavOpen(!navOpen);
+                const menu = document.querySelector(".mobile-menu");
+                menu.classList.toggle("hidden");
+              }}
+            >
+              <li
+                className="text-lg flex justify-center content-center"
+                key={route.name}
               >
-                <li
-                  className="text-lg flex justify-center content-center"
-                  key={route.name}
-                >
-                  <Link href={route.path}>{route.name}</Link>
-                </li>
-              </button>
-            ))}
-          </ul>
-        </div>
-        <h1 className="text-3xl text-center">{title}</h1>
+                <Link href={route.path}>{route.name}</Link>
+              </li>
+            </button>
+          ))}
+        </ul>
       </div>
       {
         // Desktop menu
       }
-      <div className="hidden md:flex flex-row items-center justify-between border-2 bg-gray-300 m-1 rounded-md">
-        <div></div>
+      <div className="hidden md:flex flex-row justify-center">
+        <div>
+          <h1 className="text-3xl text-white pr-6">Tri-City</h1>
+        </div>
         <div>
           <ul className="flex content-center justify-center space-x-10">
             {routes.map((route) => (
-              <li className="text-3xl" key={route.name}>
+              <li className="text-3xl text-white" key={route.name}>
                 <Link href={route.path}>{route.name}</Link>
               </li>
             ))}
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
@@ -94,7 +102,7 @@ const routes = [
     path: "/suspensions",
   },
   {
-    name: "Contact Us",
+    name: "Contact",
     path: "/about",
   },
 ];
